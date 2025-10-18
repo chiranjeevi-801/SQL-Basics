181c1a390bea364c1dfd381cf5d2bfbc34eb9306
chiranjeevi-801 <chiranjeevichiranjeevi6786@gmail.com>
2025-10-18 11:55:19 +0530

    left join in SQl

---
+++ SQL-LEFT join.sql
@@ -0,0 +1,26 @@
+
+
+select * from table1
+
+select * from table2
+
+
+select * from table1 left join table2
+on table1.c1=table2.c1
+            -- OR ---
+select * from table1 left outer join table2
+on table1.c1=table2.c1
+
+
+select * from table1 a left join table2 b 
+on a.c1=b.c1
+
+select a.c1,a.c2,b.c3 from table1 a left join table2 b 
+on a.c1=b.c1
+
+
+
+
+
+
+
