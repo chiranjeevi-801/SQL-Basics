b3b29da263473dd7aebe52a6e9f8488473a8ff3f
chiranjeevi-801 <chiranjeevichiranjeevi6786@gmail.com>
2025-10-21 18:02:19 +0530

    uses of Data Types in SQL

---
+++ SQL-Data types.sql
@@ -0,0 +1,27 @@
+
+
+--SQL Data Types
+
+--String Data 
+--char,varchar,nchar,nvarchar
+
+--M(Male) or F(Female), char(1)
+--varchar(256)
+--nchar(1),it supports unicode
+--nvarchar(256),it supports unicode
+
+--Number Types 
+   --int--->8(bytes)
+   --bigint-->4
+   --tinyint--> 1
+   --Smallint-->2
+   --decimal(presistion(p),scalar(s))
+
+--Age of the students,tinyint
+--Age of trees/buildings,smallint
+--decimal(p,s),p represents total digits, s refers to number of digits you may after decimal
+--999.99,decimal(5,20
+
+--Data->data type
+--time->time data type
+--datetime->datetime datatype
