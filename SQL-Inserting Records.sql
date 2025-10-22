c22f4a35a824b94839279af5daccf68e5e203eca
chiranjeevi-801 <chiranjeevichiranjeevi6786@gmail.com>
2025-10-22 18:38:55 +0530

    Inserting Records All Coluns and Certain Columns in a table

---
+++ SQL-Inserting Records.sql
@@ -0,0 +1,25 @@
+
+
+
+select * from student_details
+
+insert into student_details
+values( 'Abhilash','M',22,'2024-11-08',324.66),
+( 'suhas','M',22,'2024-11-08',115.35)
+
+select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME like 'student_details'
+
+
+insert into student_details(Student_Name,Gender)
+values('Priya','F')
+
+select * from student_details
+
+------------------------------------------------------------------------------------------------------
+
+insert into student_details(Age,Event_date)
+values ( 34,'2024-05-04'),(44,'2019-01-01'),(20,'2022-08-08')
+
+
+select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME like 'student_details'
+
